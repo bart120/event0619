093abcdd97c585e7dd35dc20ed4c547765a6d051
Vincent <vleclerc@inow.fr>
2019-06-03 12:02:04 +0200

    mise en page events index

--- src/app/events/events-index/events-index.page.ts
+++ src/app/events/events-index/events-index.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from 'src/app/models/event.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-events-index',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsIndexPage implements OnInit {
 
-  constructor() { }
+  listEvents: Array<Event>;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.listEvents = [
+      { title: 'test', eventDate: new Date(2019, 5, 14) },
+      { title: 'test2', eventDate: new Date(2019, 5, 16) },
+      { title: 'test3', eventDate: new Date(2019, 5, 24) }
+    ];
   }
 
+  detailEvent(id: number): void {
+    this.router.navigate([`/events/detail/${id}`]);
+  }
 }
