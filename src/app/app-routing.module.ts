654ecca00c24a35a13470d80c5fdf21f1292a70e
Vincent <vleclerc@inow.fr>
2019-06-03 12:48:11 +0200

    mise en page page de connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'events', loadChildren: './events/events.module#EventsModule' },
   { path: 'authentication', loadChildren: './authentication/authentication.module#AuthenticationPageModule' }
