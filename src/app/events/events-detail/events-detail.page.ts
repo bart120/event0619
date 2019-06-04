3bd634dafc86a31fe82aa507222edce9b4e2e63a
Vincent <vleclerc@inow.fr>
2019-06-04 12:16:03 +0200

    ajout toast

--- src/app/events/events-detail/events-detail.page.ts
+++ src/app/events/events-detail/events-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventsService } from 'src/app/services/events.service';
 import { Event } from 'src/app/models/event.model';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-events-detail',
@@ -12,11 +13,23 @@ export class EventsDetailPage implements OnInit {
 
   event: Event;
 
-  constructor(private route: ActivatedRoute, private servEvent: EventsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private servEvent: EventsService,
+    private nav: NavController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
     this.servEvent.getEventById(this.route.snapshot.params['id']).subscribe(
-      data => this.event = data
+      data => this.event = data,
+      async err => {
+        this.nav.back();
+        const toast = await this.toastController.create({
+          message: 'Existe pas.',
+          duration: 2000
+        });
+        toast.present();
+      }
     );
 
   }
